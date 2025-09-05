728bc8f9dc6932ac0cb22ff76685828593afaab2
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-09-05 18:54:43 -0400

    gug

--- addons/gd_test/plugin.gd
+++ addons/gd_test/plugin.gd
@@ -5,7 +5,7 @@ var dock: Control
 
 func _enter_tree() -> void:
 	dock = preload("res://addons/gd_test/scenes/dock.tscn").instantiate()
-	add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
+	add_control_to_dock(DOCK_SLOT_LEFT_UR, dock)
 
 func _exit_tree() -> void:
 	remove_control_from_docks(dock)
--- addons/gd_test/scripts/test_dock.gd
+++ addons/gd_test/scripts/test_dock.gd
@@ -1,13 +1,40 @@
+@tool
 extends Control
 
-@onready var path_text: TextEdit = %PathText
+@onready var _directory_label: Label = %DirectoryLabel
 
-var found_scenes: Array[PackedScene] = []
+var dialog: EditorFileDialog
 
-func _on_scan_button_pressed() -> void:
-	if path_text.text.is_empty():
-		push_warning("The given path is empty!")
+var _target_folder: String = "res://scenes/test/":
+	set(value):
+		_target_folder = value
+		_directory_label.text = _get_target_folder_text(value)
+
+func _ready() -> void:
+	_directory_label.text = _get_target_folder_text(_target_folder)
+
+func _get_target_folder_text(dir: String) -> String:
+	return "Current Directory: " + dir
+
+func _on_folder_select_button_pressed() -> void:
+	if dialog != null:
 		return
+
+	dialog = EditorFileDialog.new()
+	dialog.file_mode = EditorFileDialog.FILE_MODE_OPEN_DIR
+
+	var screen_size := DisplayServer.screen_get_size()
 	
-	var found_scenes = ResourceUtil.find_scenes(path_text.text)
-	print_debug("gug")
+	dialog.size = screen_size / 2
+
+	add_child(dialog)
+
+	dialog.move_to_center()
+	dialog.show()
+
+	dialog.dir_selected.connect(_on_folder_select_dialog_dir_selected)
+
+func _on_folder_select_dialog_dir_selected(dir: String) -> void:
+	_target_folder = dir + "/"
+	dialog.queue_free()
+	dialog = null
