b6630d70312443d3ad5e26e58155e4735ecb113a
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-09-05 20:02:07 -0400

    done

--- addons/gd_test/scripts/test_dock.gd
+++ addons/gd_test/scripts/test_dock.gd
@@ -1,7 +1,12 @@
 @tool
 extends Control
 
+signal run_scene(path: String)
+
 @onready var _directory_label: Label = %DirectoryLabel
+@onready var test_group_vbox: VBoxContainer = %TestGroupVBox
+
+const TEST_GROUP_SCENE: PackedScene = preload("res://addons/gd_test/scenes/test_group.tscn")
 
 var dialog: EditorFileDialog
 
@@ -13,13 +18,6 @@ var _target_folder: String = "res://scenes/test/":
 func _ready() -> void:
 	_directory_label.text = _get_target_folder_text(_target_folder)
 
-func _get_target_folder_text(dir: String) -> String:
-	return "Current Directory: " + dir
-
-func _on_folder_select_button_pressed() -> void:
-	if dialog != null:
-		return
-
 	dialog = EditorFileDialog.new()
 	dialog.file_mode = EditorFileDialog.FILE_MODE_OPEN_DIR
 
@@ -29,12 +27,36 @@ func _on_folder_select_button_pressed() -> void:
 
 	add_child(dialog)
 
+	_on_folder_select_dialog_dir_selected(_target_folder)
+
+	visibility_changed.connect(func(): _on_folder_select_dialog_dir_selected(_target_folder))
+
+func _get_target_folder_text(dir: String) -> String:
+	return "Current Directory: " + dir
+
+func _on_folder_select_button_pressed() -> void:
 	dialog.move_to_center()
 	dialog.show()
 
 	dialog.dir_selected.connect(_on_folder_select_dialog_dir_selected)
 
 func _on_folder_select_dialog_dir_selected(dir: String) -> void:
-	_target_folder = dir + "/"
-	dialog.queue_free()
-	dialog = null
+	if dir[-1] != "/":
+		_target_folder = dir + "/"
+	else:
+		_target_folder = dir
+
+	for child in test_group_vbox.get_children():
+		test_group_vbox.remove_child(child)
+		child.queue_free()
+	
+	var scenes = ResourceUtil.find_scenes(_target_folder)
+	for scene in scenes:
+		var group := TEST_GROUP_SCENE.instantiate()
+		group.text = scene.resource_path
+		group.get_node("Button").pressed.connect(_run_test_group.bind(group))
+		test_group_vbox.add_child(group)
+
+func _run_test_group(test_group: Control) -> void:
+	var path: String = test_group.text
+	run_scene.emit(path)
